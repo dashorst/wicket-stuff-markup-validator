5839b2b995121adf8f01239d08e9149b5646c445
Martijn Dashorst <dashorst@macbookpro.local>
2010-01-28 13:53:33 +0100

    Fix rendered output for stoopid wicket:ignoreIfNotActive bug, otherwise the HTML validator will choke with a fatal error and not discover other issues in your markup

--- src/main/java/org/wicketstuff/htmlvalidator/HtmlValidationResponseFilter.java
+++ src/main/java/org/wicketstuff/htmlvalidator/HtmlValidationResponseFilter.java
@@ -11,43 +11,61 @@ import org.tuckey.web.filters.validation.LineIssue;
 import org.tuckey.web.filters.validation.ValidationHandler;
 import org.tuckey.web.filters.validation.generated.GeneratedValidationReport;
 
-public class HtmlValidationResponseFilter implements IResponseFilter {
-	
+public class HtmlValidationResponseFilter implements IResponseFilter
+{
+
 	private boolean ignoreKnownWicketBugs;
-	
+
 	private boolean ignoreAutocomplete;
-	
-	public AppendingStringBuffer filter(AppendingStringBuffer responseBuffer) {
+
+	public AppendingStringBuffer filter(AppendingStringBuffer responseBuffer)
+	{
 		Page responsePage = RequestCycle.get().getResponsePage();
-		
+
 		// when the responsepage is an error page, don't filter the page
-		if (responsePage == null || responsePage.isErrorPage()) {
+		if (responsePage == null || responsePage.isErrorPage())
+		{
 			return responseBuffer;
 		}
 
 		String response = responseBuffer.toString();
 
-		if (isXHtml(responseBuffer)) {
-			try {
-				ValidationHandler handler = new ValidationHandler(response, "") {
+		if (isXHtml(responseBuffer))
+		{
+			if (isIgnoreKnownWicketBugs())
+			{
+				response =
+					response.replaceAll("&wicket:ignoreIfNotActive",
+						"&amp;wicket:ignoreIfNotActive");
+			}
+			try
+			{
+				ValidationHandler handler = new ValidationHandler(response, "")
+				{
 					@Override
-					protected boolean isFalseError(LineIssue lineIssue) {
+					protected boolean isFalseError(LineIssue lineIssue)
+					{
 						if (isIgnoreKnownWicketBugs() && isKnownWicketBug(lineIssue))
 							return true;
 						if (isIgnoreAutocomplete() && isAutocompleteError(lineIssue))
 							return true;
-						
+
 						return super.isFalseError(lineIssue);
 					}
 				};
 				handler.parse();
-				if (!handler.isValid()) {
+				if (!handler.isValid())
+				{
 					insertMarkup(responseBuffer, handler);
 				}
-			} catch (FileNotFoundException e) {
+			}
+			catch (FileNotFoundException e)
+			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (IOException e) {
+			}
+			catch (IOException e)
+			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -55,50 +73,59 @@ public class HtmlValidationResponseFilter implements IResponseFilter {
 		return responseBuffer;
 	}
 
-	public void setIgnoreAutocomplete(boolean ignoreAutocomplete) {
+	public void setIgnoreAutocomplete(boolean ignoreAutocomplete)
+	{
 		this.ignoreAutocomplete = ignoreAutocomplete;
 	}
 
-	public boolean isIgnoreAutocomplete() {
+	public boolean isIgnoreAutocomplete()
+	{
 		return ignoreAutocomplete;
 	}
-	
-	public void setIgnoreKnownWicketBugs(boolean ignoreKnownWicketBugs) {
+
+	public void setIgnoreKnownWicketBugs(boolean ignoreKnownWicketBugs)
+	{
 		this.ignoreKnownWicketBugs = ignoreKnownWicketBugs;
 	}
 
-	public boolean isIgnoreKnownWicketBugs() {
+	public boolean isIgnoreKnownWicketBugs()
+	{
 		return ignoreKnownWicketBugs;
 	}
-	
-	private boolean isAutocompleteError(LineIssue lineIssue) {
+
+	private boolean isAutocompleteError(LineIssue lineIssue)
+	{
 		return "Attribute &quot;autocomplete&quot; must be declared for element type &quot;input&quot;."
-				.equals(lineIssue.getMessage());
+			.equals(lineIssue.getMessage());
 	}
-	
-	private boolean isKnownWicketBug(LineIssue lineIssue) {
+
+	private boolean isKnownWicketBug(LineIssue lineIssue)
+	{
 		return isWicket2033(lineIssue) || isWicket2316(lineIssue);
 	}
-	
-	private boolean isWicket2033(LineIssue lineIssue) {
+
+	private boolean isWicket2033(LineIssue lineIssue)
+	{
 		return "The reference to entity &quot;wicket:ignoreIfNotActive&quot; must end with the ';' delimiter."
-				.equals(lineIssue.getMessage());
+			.equals(lineIssue.getMessage());
 	}
-	
-	private boolean isWicket2316(LineIssue lineIssue) {
+
+	private boolean isWicket2316(LineIssue lineIssue)
+	{
 		return "The entity name must immediately follow the '&amp;' in the entity reference."
-				.equals(lineIssue.getMessage());
+			.equals(lineIssue.getMessage());
 	}
-	
-	private boolean isXHtml(AppendingStringBuffer response) {
+
+	private boolean isXHtml(AppendingStringBuffer response)
+	{
 		int maxLength = Math.min(response.length(), 128);
 		String contentSoFar = response.substring(0, maxLength);
 		return contentSoFar.indexOf("<!DOCTYPE") != -1
-				&& contentSoFar.indexOf("-//W3C//DTD XHTML") != -1;
+			&& contentSoFar.indexOf("-//W3C//DTD XHTML") != -1;
 	}
 
-	private void insertMarkup(AppendingStringBuffer original,
-			ValidationHandler handler) {
+	private void insertMarkup(AppendingStringBuffer original, ValidationHandler handler)
+	{
 
 		GeneratedValidationReport report = new GeneratedValidationReport();
 		report.validationHandler = handler;
