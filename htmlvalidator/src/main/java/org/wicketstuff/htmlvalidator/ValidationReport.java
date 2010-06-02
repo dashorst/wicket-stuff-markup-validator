896c78ef2a9108f38139d92d9ed737422280e5b6
thuis <thuis@papegaaij.(none)>
2010-06-02 08:33:50 +0200

    A little more report

--- htmlvalidator/src/main/java/org/wicketstuff/htmlvalidator/ValidationReport.java
+++ htmlvalidator/src/main/java/org/wicketstuff/htmlvalidator/ValidationReport.java
@@ -3,6 +3,8 @@ package org.wicketstuff.htmlvalidator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.ResourceReference;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXParseException;
 
@@ -35,11 +37,45 @@ public class ValidationReport implements ErrorHandler
 
 	public String getHeadMarkup()
 	{
-		return "";
+		return "    <link rel=\"stylesheet\" type=\"text/css\" href=\""
+			+ RequestCycle.get().urlFor(
+				new ResourceReference(ValidationReport.class, "validator.css")) + "\" />\n";
 	}
 
 	public String getBodyMarkup()
 	{
-		return parseErrors.toString();
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div class=\"validation-report\">");
+		generateErrorList(sb);
+		sb.append("</div>");
+		return sb.toString();
+	}
+
+	private void generateErrorList(StringBuilder sb)
+	{
+		sb.append("<ul>\n");
+		for (SAXParseException curError : parseErrors)
+		{
+			generateErrorItem(sb, curError);
+		}
+		sb.append("</ul>\n");
+	}
+
+	private void generateErrorItem(StringBuilder sb, SAXParseException error)
+	{
+		sb.append("<li><span class=\"position\">");
+		if (error.getLineNumber() == -1 && error.getColumnNumber() == -1)
+		{
+			sb.append("-");
+		}
+		else
+		{
+			sb.append(error.getLineNumber());
+			sb.append(":");
+			sb.append(error.getColumnNumber());
+		}
+		sb.append("</span> <span class=\"message\">");
+		sb.append(error.getLocalizedMessage());
+		sb.append("</span></li>\n");
 	}
 }
